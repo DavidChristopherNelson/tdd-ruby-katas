78c6e21a44e16745cc22b0733d2796181de94a0c
David Nelson <david.nelson@dubber.net>
2022-06-02 11:08:03 +1000

    Finish task.

--- lib/string_average.rb
+++ lib/string_average.rb
@@ -1,5 +1,25 @@
 class StringAverage
+  @words_to_digits = {
+    'zero' => 0,
+    'one' => 1,
+    'two' => 2,
+    'three' => 3,
+    'four' => 4,
+    'five' => 5,
+    'six' => 6,
+    'seven' => 7,
+    'eight' => 8,
+    'nine' => 9
+  }
+
   def self.calculate(string)
-    ""
+    array_of_words = string.split(' ')
+    sum = 0
+    array_of_words.each do |word|
+      return 'n/a' unless @words_to_digits.keys.include? word
+      sum += @words_to_digits[word]
+    end
+    average = (sum / array_of_words.size).floor()
+    @words_to_digits.key(average)
   end
 end
--- spec/spec_string_average.rb
+++ spec/spec_string_average.rb
@@ -4,7 +4,35 @@ require_relative '../lib/string_average.rb'
 
 
 describe StringAverage do
-  it 'should return a String' do
-    expect(StringAverage.calculate("one")).to be_a String
+  context 'calculate' do
+    let(:allowed_values) {%w{zero one two three four five six seven eight nine}}
+
+    it 'should return a String' do
+      expect(StringAverage.calculate("one")).to be_a String
+    end
+
+    it 'should return n/a if ten is entered' do
+      expect(StringAverage.calculate('ten')).to eq('n/a')
+    end
+
+    it 'should return n/a if twentytwo is entered' do
+      expect(StringAverage.calculate('twentytwo')).to eq('n/a')
+    end
+
+    it 'should return n/a if xdkaiwe234s is entered' do
+      expect(StringAverage.calculate('xdkaiwe234s')).to eq('n/a')
+    end
+
+    it 'should return an allowed value' do
+      result = StringAverage.calculate('zero nine five')
+      expect(allowed_values.include?(result)).to be true
+    end
+
+    it 'should return the correct value' do
+      expect(StringAverage.calculate('zero nine five two')).to eq('four')
+      expect(StringAverage.calculate('six nine five')).to eq('six')
+      expect(StringAverage.calculate('eight nine five two seven zero one'))
+        .to eq('four')
+    end
   end
 end
